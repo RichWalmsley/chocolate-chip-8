2c42742c5afeefc2bab0909574f383494de6963b
RichWalmsley <r.walmsley97@gmail.com>
2025-04-05 18:19:36 -0230

    Added drawing display buffer to screen and UI for screen

--- src/main.c
+++ src/main.c
@@ -2,15 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "chip8.h"
 
 #define ROM "roms/IBM Logo.ch8"
 
-#define BLACK "\x1b[48;2;0;0;0m "
-#define WHITE "\x1b[48;2;255;255;255m "
-#define NEWLINE "\x1b[0m\n"
-
 chip8_t emulator;
+WINDOW *win;
+
+void draw(chip8_t *chip8)
+{
+    for (int y = 0; y < HEIGHT; y++)
+    {
+        for (int x = 0; x < WIDTH; x++)
+        {
+            if (chip8->display_buffer[x + y * WIDTH] == 0x1)
+            {
+                mvaddch(y + 1, x + 1, '#');
+            }
+            else
+            {
+                mvaddch(y + 1, x + 1, ' ');
+            }
+        }
+    }
+}
 
 int main()
 {
@@ -33,26 +49,23 @@ int main()
             break;
     }
 
-    initscr();
+    win = initscr();
     cbreak();
     noecho();
     clear();
-    refresh();
 
-    move(5,5);
-
-    char *text = "Hello, World!";
-    for (int i = 0; i < strlen(text); i++)
-    {
-        addch(text[i]);
-        addch(' ');
-    }
+    // Resize the initialized window and add a order
+    wresize(win, HEIGHT + 2, WIDTH + 2);
+    box(win, 0, 0);
 
     refresh();
 
     while(1)
     {
-
+        chip8_cycle(&emulator);
+        draw(&emulator);
+        usleep(10000);
+        refresh();
     }
 
     return 0;
